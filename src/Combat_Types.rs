7adf760a818c2551729f1eeac40a49c677862895
AstridIsVeryCool <146063339+AstridIsVeryCool@users.noreply.github.com>
2023-09-26 12:56:21 -0700

    Reorganized (way harder than it should have been)

--- src/Combat_Types.rs
+++ src/Combat_Types.rs
@@ -1,74 +1,69 @@
-pub mod Combat_Types
+
+use rand::Rng;
+//Enumeration representing the damage type of damage
+#[derive(Copy, Clone, Debug)]
+pub enum DamageType
 {
-    use rand::Rng;
-    //Enumeration representing the damage type of damage
-    #[derive(Copy, Clone, Debug)]
-    pub enum DamageType
-    {
-        Acid,
-        Bludgeoning,
-        Cold,
-        Fire,
-        Force,
-        Lightning,
-        Necrotic,
-        Piercing,
-        Poison,
-        Psychic,
-        Radiant,
-        Slashing,
-        Thunder
-    }
+    Acid,
+    Bludgeoning,
+    Cold,
+    Fire,
+    Force,
+    Lightning,
+    Necrotic,
+    Piercing,
+    Poison,
+    Psychic,
+    Radiant,
+    Slashing,
+    Thunder
+}
 
-    //represents the amount of damage of a certain type that could potentially be dealt
-    struct DamageCategory
-    {
-        category_type: DamageType,
-        die: i32,
-        num_dice: i32,
-        bonus: i32
-    }
+//represents the amount of damage of a certain type that could potentially be dealt
+pub struct DamageCategory
+{
+    category_type: DamageType,
+    die: i32,
+    num_dice: i32,
+    bonus: i32
+}
+impl DamageCategory
+{
 
-    impl DamageCategory
+    //Calculates the damage done in that category and returns it as an instance of Damage
+    fn roll_for_category(&self) -> Damage
     {
-
-        //Calculates the damage done in that category and returns it as an instance of Damage
-        fn roll_for_category(&self) -> Damage
+        let mut running_total = self.bonus;
+        for x in 0..self.num_dice
         {
-            let mut running_total = self.bonus;
-            for x in 0..self.num_dice
-            {
-                running_total += rand::thread_rng().gen_range(1..=self.die);
-            }
-            return Damage { damage_type_for_damage: (self.category_type), amount: (running_total) }
+            running_total += rand::thread_rng().gen_range(1..=self.die);
         }
+        return Damage { damage_type_for_damage: (self.category_type), amount: (running_total) }
     }
+}
 
-    pub struct Damage
+pub struct Damage
+{
+    pub damage_type_for_damage: DamageType,
+    pub amount: i32,
+}
+
+pub struct Attack
+{
+    pub to_hit_bonus: i32,
+    pub damage_categories: Vec<DamageCategory>
+}
+
+impl Attack
+{
+    //fn roll_damage(&self) -> Damage
+    //{
+        
+    //}
+    
+    fn roll_to_hit(&self, ac: &i32) -> bool
     {
-        damage_type_for_damage: DamageType,
-        amount: i32,
-    }
-
-    pub struct Attack
-    {
-        to_hit_bonus: i32,
-        damage_categories: Vec<DamageCategory>
-    }
-
-    impl Attack
-    {
-        fn roll_damage(&self) -> Damage
-        {
-            
-        }
-
-        fn roll_to_hit(&self, ac: &i32) -> bool
-        {
-            
-            let roll = rand::thread_rng().gen_range(1..=20);
-            
-            return (roll + self.to_hit_bonus) >= *ac;
-        }
+        let roll = rand::thread_rng().gen_range(1..=20);
+        return (roll + self.to_hit_bonus) >= *ac;
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,15 @@
-use std::io;
-use rand::Rng;
-mod Combat_Types;
+mod combat_types;
 
-fn main() {
-    let a: Combat_Types::Attack = Attack
+fn main() 
+{
+    let a: combat_types::Attack = combat_types::Attack
     {
         to_hit_bonus: 3, 
+        damage_categories: vec![]
     };
 
-    let b: Damage = a.roll_damage();
+    //let b: Damage = a.roll_damage();
 
-    println!("{:?}", b.Combat_types::damage_type_for_damage);
-    println!("{}", b.Combat_types::amount);
+    //println!("{:?}", b.damage_type_for_damage);
+    //println!("{}", b.amount);
 }
