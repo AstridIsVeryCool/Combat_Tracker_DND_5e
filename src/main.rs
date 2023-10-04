5b96ed735899f8ae46a9ed4c96b5b73633311cda
AstridIsVeryCool <146063339+AstridIsVeryCool@users.noreply.github.com>
2023-10-03 18:44:00 -0700

    part of create entity completed

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,9 @@
 use std::{io, string};
+use std::io::Read;
 use combat_types::{Damage, DamageCategory, Entity};
 use log::debug;
+use crate::combat_types::Attack;
+
 #[allow(unused)]
 
 mod combat_types;
@@ -38,24 +41,59 @@ fn create_entity() //-> Entity
     let is_player: bool = (user_input == "y\r\n") || (user_input == "Y\r\n");
 
     println!("How many hit points does it currently have?: enter a positive integer");
+    user_input = "".to_string();
     io::stdin().read_line(&mut user_input);
-    let current_hit_points: i32 = user_input.parse().unwrap();
+    let current_hit_points: i32 = user_input[0..user_input.len()-2].parse().unwrap();
 
     println!("What is its maximum hit points?: enter a positive integer");
+    user_input = "".to_string();
     io::stdin().read_line(&mut user_input);
-    let maximum_hit_points: i32 = user_input.parse().unwrap();
-
-    println!("What are its maximum hit points?: enter a positive integer");
-    io::stdin().read_line(&mut user_input);
-    let maximum_hit_points: i32 = user_input.parse().unwrap();
+    let maximum_hit_points: i32 = user_input[0..user_input.len()-2].parse().unwrap();
 
     println!("What are its temporary hit points?: enter a positive integer");
+    user_input = "".to_string();
     io::stdin().read_line(&mut user_input);
-    let temporary_hit_points: i32 = user_input.parse().unwrap();
+    let temporary_hit_points: i32 = user_input[0..user_input.len()-2].parse().unwrap();
 
     println!("What is its armor class?: enter a positive integer");
+    user_input = "".to_string();
     io::stdin().read_line(&mut user_input);
-    let ac: i32 = user_input.parse().unwrap();
+    let ac: i32 = user_input[0..user_input.len()-2].parse().unwrap();
 
+    let mut more_attacks = true;
+
+    let attacks = create_attack_vec();
+}
+
+fn create_attack_vec() -> Vec<Attack>
+{
+    let user_input: String::new();
+
+    let more_attacks: bool;
+
+    let attacks: Vec<combat_types::DamageCategory> vec![];
+    while more_attacks
+    {
+        println!("What is the name of this attack?:");
+        user_input = "".to_string();
+        io::stdin().read_line((&mut user_input));
+        let mut attack_name = &user_input[0..user_input.len() - 5];
+        dbg!(attack_name);
+
+        println!("What is the to hit bonus of this attack?");
+        user_input = "".to_string();
+        io::stdin().read_line(&mut user_input);
+        let mut to_hit_bonus: i32 = user_input[0..user_input.len()-2].parse().unwrap();
+
+        println!("Is there another attack?: y/n");
+        user_input = "".to_string();
+        io::stdin().read_line(&mut user_input);
+        more_attacks = (user_input == "y\r\n") || (user_input == "Y\r\n");
+    }
+    return vec![];
+}
+
+fn create_damage_category()
+{
 
 }
